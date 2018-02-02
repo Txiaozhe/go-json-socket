30ed1c6c370560bb45747b5b6f1e635f35f89c8d
TangXiaoji <txiaozhe@gmail.com>
2018-02-02 23:03:32 +0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,39 @@
+# go-json-socket
+
+## A simple RPC tool, because RPC is needed to communicate with go services and node services in the encoding process, and [node-json-socket](https://github.com/sebastianseilund/node-json-socket) library is used for RPC communication between node services, so this library is written for better compatibility with node services.
+## 一个简易的rpc工具，由于编码过程中需要使用rpc进行go服务和node服务的通信，而node服务之间使用[node-json-socket](https://github.com/sebastianseilund/node-json-socket)库来进行rpc通信，为了更好地与node服务兼容，因此写了这个库。
+
+## quick start
+
+```go
+func main() {
+	conn, err := jsonsocket.Connect("127.0.0.1:9838")
+	if err != nil {
+		log.Println("connect error: ", err)
+	}
+
+	token_info := Token{
+		"Bearer eyJ1aWQiOjEyNTE4NDY3NDgsInR5cGUiOiJ0b29sIn0=.NWRhMWNlOTRiMTFjNmQwODM5YjA2Y2E5ZjZjMTBkZGQ0NDVjMjg2ZjlhNmY0MjkyYTExNWNhNDA1ZGZiNDQyNA==",
+	}
+	auth_info := Auth{
+		"tool",
+		"auth_request",
+		token_info,
+	}
+
+	ch, err := jsonsocket.SendMessage(conn, auth_info)
+	if err != nil {
+		log.Println("send msg error: ", err)
+	}
+
+	log.Println(<-ch)
+
+	ch1, err := jsonsocket.HandleMessage(conn)
+	if err != nil {
+		log.Println("handle msg error: ", err)
+	}
+
+	res := <-ch1
+	log.Println(res, res.Len, res.Data)
+}
+```
---
+++ json_socket.go
@@ -0,0 +1,82 @@
+package gojsonsocket
+
+import (
+	"bytes"
+	"encoding/json"
+	"log"
+	"net"
+	"regexp"
+	"strconv"
+	"time"
+)
+
+type Response struct {
+	Len  int         `json:"len"`
+	Data interface{} `json:"data"`
+}
+
+func Connect(addr string) (net.Conn, error) {
+	conn, err := net.DialTimeout("tcp", "127.0.0.1:9838", 15*time.Second)
+	if err != nil {
+		return nil, err
+	}
+
+	return conn, nil
+}
+
+func SendMessage(conn net.Conn, d interface{}) (chan int, error) {
+	ch := make(chan int)
+
+	jsonData, err := json.Marshal(d)
+	if err != nil {
+		return nil, err
+	}
+
+	delimeter := []byte("#")
+	length := []byte(strconv.Itoa(len(jsonData)))
+
+	data := bytesCombine(length, delimeter, jsonData)
+
+	go func() {
+		byteLen, err := conn.Write(data)
+		if err != nil {
+			log.Println("socket write error:", err)
+			ch <- -1
+		}
+
+		ch <- byteLen
+	}()
+
+	return ch, nil
+}
+
+func HandleMessage(conn net.Conn) (chan Response, error) {
+	ch := make(chan Response)
+
+	buf := make([]byte, 1024)
+
+	go func() {
+		_, err := conn.Read(buf)
+		if err != nil {
+			ch <- Response{0, err}
+		}
+
+		reg := regexp.MustCompile(`(\S*)#(\S*)`)
+
+		str := reg.FindStringSubmatch(string(buf))
+
+		dataLen, err := strconv.Atoi(str[1])
+		if err != nil{
+			ch <- Response{0, err}
+		}
+
+		res := Response{dataLen, str[2]}
+		ch <- res
+	}()
+
+	return ch, nil
+}
+
+func bytesCombine(pBytes ...[]byte) []byte {
+	return bytes.Join(pBytes, []byte(""))
+}
